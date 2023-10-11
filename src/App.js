1254acb50ccaac41e0516e03983e5170dba5968f
Yomi Eluwande <yomi.eluwande@gmail.com>
2023-10-11 23:41:31 +0200

    React Suspense with React.lazy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react";
 
 import "./App.css";
-import Shows from "./components/Shows";
+
+const Shows = React.lazy(() => import("./components/Shows"));
 
 function App() {
   return (
